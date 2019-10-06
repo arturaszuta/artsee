df8594d0df882dc0c83217df2ee7ecd1a62d2391
ahrke <david.duy.minh@gmail.com>
2019-10-06 10:00:32 -0400

    add flatlist for profilescreen sections

--- src/Components/ProfileScreen/Section.js
+++ src/Components/ProfileScreen/Section.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Image, View, Dimensions } from 'react-native';
-import { TouchableHighlight } from 'react-native';
+import { View, Dimensions } from 'react-native';
+import { TouchableHighlight, FlatList } from 'react-native';
 
 import CachedImage from '../../helpers/CachedImage';
 
@@ -8,33 +8,47 @@ let {width, height} = Dimensions.get("window");
 
 
 export default Section = ({navigation, section}) => {
+  const data = section.map((art, idx) => {
+    const imgUrl = 'https://arzmkdmkzm.cloudimg.io/width/' + 200 + '/x/' + art.img_url;
+    return {
+      id: art.id,
+      imgUrl,
+      art,
+      idx
+    };
+  })
+
+  const Item = ({art, imgUrl, idx}) => {
+    return (
+      <View
+        key={art.imgUrl}
+        style={[
+          { width: width / 3 },
+          { height: width / 3 },
+          { marginBottom: 2 },
+          idx % 3 !== 0 ? { paddingLeft: 2 } : { paddingLeft: 0 },
+        ]}
+      >
+        <TouchableHighlight style={{ flex: 1 }} activeOpacity={0} onPress={() => {
+            navigation.navigate('ArtModal', {art: art})}
+          } 
+        >
+          <CachedImage
+            style={{ flex: 1, resizeMode: 'cover', width: "100%" }}
+            source={imgUrl}
+          />
+        </TouchableHighlight>
+      </View>
+    )
+  }
+
   return (
-    <View style={{ flexDirection: "row", flexWrap: "wrap", flex: 1 }}>
-      {section.map((art, idx) => {
-        const imgUrl = 'https://arzmkdmkzm.cloudimg.io/width/' + 200 + '/x/' + art.img_url;
-        return (
-          <View
-            key={art.id}
-            style={[
-              { width: width / 3 },
-              { height: width / 3 },
-              { marginBottom: 2 },
-              idx % 3 !== 0 ? { paddingLeft: 2 } : { paddingLeft: 0 },
-            ]}
-          >
-            <TouchableHighlight style={{ flex: 1 }} activeOpacity={0} onPress={() => {
-                console.log("==|==> image clicked")
-                navigation.navigate('ArtModal', {art: art})}
-              } 
-            >
-              <CachedImage
-                style={{ flex: 1, resizeMode: 'cover', width: "100%" }}
-                source={imgUrl}
-              />
-            </TouchableHighlight>
-          </View>
-        );
-      })}
-    </View>
+    <FlatList
+      style={{margin:2}}
+      numColumns={3}                   
+      data={data}
+      renderItem={({item}) => <Item imgUrl={item.imgUrl} art={item.art} idx={item.idx} />}
+      keyExtractor={(item) => item.id}
+    />
   )
 }
