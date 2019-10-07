b069fb5caf32f2afdf9f1e7c2b8f53679d3a8125
Arturas Zuta <arturaszuta@gmail.com>
2019-10-07 12:38:40 -0400

    filter testing

--- src/Components/FeedScreen/FeedScreen.js
+++ src/Components/FeedScreen/FeedScreen.js
@@ -2,6 +2,7 @@ import * as WebBrowser from 'expo-web-browser';
 import React, { useState } from 'react';
 import useApplicationData from '../../hooks/useApplicationData';
 
+
 import { Container, Header, Content, Root, Left, Body, Button, Icon, Segment, Text } from "native-base";
 
 import Deck from './Deck';
@@ -16,9 +17,17 @@ export default function FeedScreen() {
 
   const [activeFilter, setActiveFilter] = useState('default');
 
+  const initialState = state.arts;
+
   const changeFeed = async function(param) {
     setActiveFilter(param)
-    
+    if (param === 'default') {
+      state.arts = initialState;
+    } else {
+      
+      fetch('https://artsee-back-end.herokuapp.com/api/mostlikedart?method=' + param + '&user_id=' + state.user.id).then(result => result.json()).then(res => {state.arts = res});
+    }
+  
   }
 
 
@@ -36,7 +45,7 @@ export default function FeedScreen() {
               <Button active={activeFilter === 'default' ? true : null} first onPress={() => { changeFeed('default')}}><Text>Default</Text></Button>
               <Button active={activeFilter === 'liked' ? true : null} onPress={() => { changeFeed('liked')}} ><Text>Liked</Text></Button>
               <Button active={activeFilter === 'visited' ? true : null} onPress={() => {changeFeed('visited') }}><Text>Visited</Text></Button>
-              <Button active={activeFilter === 'seenlist' ? true : null} last onPress={() => {changeFeed('seenlist') }}><Text>Seen</Text></Button>
+              <Button active={activeFilter === 'seelist' ? true : null} last onPress={() => {changeFeed('seelist') }}><Text>Seen</Text></Button>
             </Segment>
           </Body>
         </Header>
